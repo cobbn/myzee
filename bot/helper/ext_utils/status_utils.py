2bf21f27c131044c24ec92c657be4f28bf25b8f2
cobbn <168705970+cobbn@users.noreply.github.com>
2024-11-21 23:16:07 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -274,8 +274,7 @@ async def get_readable_message(
                 else task.progress()
             )
             msg += (
-                f"\n{get_progress_bar_string(progress)} » <b>{progress} » {task.speed()}</b>"
-                f"\n<code>Status :</code> <b>{tstatus}</b>"
+                f"\n{get_progress_bar_string(progress)} » <b>{tstatus} » {task.speed()}</b>"
                 f"\n<code>Done   :</code> {task.processed_bytes()} of {task.size()}"
                 f"\n<code>ETA    :</code> {task.eta()}"
                 f"\n<code>Past   :</code> {elapsed}"
